701fa577062e0367d713a1f296c84d698e49bc29
Sami Viitala <sami.viitala@devved.fi>
2025-04-11 23:12:02 +0300

    Initial

---
+++ DotNet/Hetut/Hetut.Test/SsnValidatorTests.cs
@@ -0,0 +1,89 @@
+namespace Hetut.Test;
+
+public class Tests
+{
+   
+    [TestCaseSource(nameof(ValidSsnTestCases))]
+    public void Validate_GivenValidSsn_ReturnsTrue(string ssn)
+    {
+        var sut = new SsnValidator();
+        var result = sut.Validate(ssn);
+        Assert.That(result, Is.True, $"Expected true for valid SSN: {ssn}");
+    }
+    
+    [TestCaseSource(nameof(TooShortInvalidSsnTestCases))]
+    [TestCaseSource(nameof(TooLongInvalidSsnTestCases))]
+    [TestCaseSource(nameof(WrongChecksumInvalidSsnTestCases))]
+    public void Validate_GivenInvalidSsn_ReturnsFalse(string ssn)
+    {
+        var sut = new SsnValidator();
+        var result = sut.Validate(ssn);
+        Assert.That(result, Is.False, $"Expected false for invalid SSN: {ssn}");
+    }
+    
+    public static IEnumerable<object> TooShortInvalidSsnTestCases
+    {
+        get
+        {
+            yield return "0";
+            yield return "0105";
+            yield return "020594X903";
+        }
+    }
+    
+    public static IEnumerable<object> TooLongInvalidSsnTestCases
+    {
+        get
+        {
+            yield return "010594Y90322";
+            yield return "010594Y90322010594Y90322";
+        }
+    }
+
+    public static IEnumerable<object> WrongChecksumInvalidSsnTestCases
+    {
+        get
+        {
+            yield return "010594Y9031";
+            yield return "010594Y9022";
+            yield return "020594X9033";
+        }
+    }
+    
+    // public static IEnumerable<object> WrongNNNPartInvalidSsnTestCases
+    // {
+    //     get
+    //     {
+    //         // These cases have valid checksum but invalid NNN value
+    //         yield return "010594Y9031";
+    //     }
+    // }
+
+    public static IEnumerable<object> ValidSsnTestCases
+    {
+        get
+        {
+            yield return "010594Y9032";
+            yield return "010594Y9021";
+            yield return "020594X903P";
+            yield return "020594X902N";
+            yield return "030594W903B";
+            yield return "030694W9024";
+            yield return "040594V9030";
+            yield return "040594V902Y";
+            yield return "050594U903M";
+            yield return "050594U902L";
+            yield return "010516B903X";
+            yield return "010516B902W";
+            yield return "020516C903K";
+            yield return "020516C902J";
+            yield return "030516D9037";
+            yield return "030516D9026";
+            yield return "010501E9032";
+            yield return "020502E902X";
+            yield return "020503F9037";
+            yield return "020504A902E";
+            yield return "020504B904H";
+        }
+    }
+}
---
+++ DotNet/Hetut/Hetut/SsnValidator.cs
@@ -0,0 +1,184 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Hetut;
+
+/// <summary>
+///     Validator for Finnish social security number.
+/// </summary>
+public interface ISsnValidator
+{
+    /// <summary>
+    ///     Validates the given Finnish social security number.
+    ///     This method efficiently checks the format and structure of the SSN, only returning a boolean value indicating validity of given SSN.
+    ///     <remarks>
+    ///     Considers test SSNs where NNN is 900-999 as valid.
+    ///     </remarks>
+    /// </summary>
+    /// <param name="ssn">Finnish social security number to validate.</param>
+    /// <returns>True if the given Finnish social security number is valid; otherwise, false.</returns>
+    bool Validate([NotNullWhen(true)] string? ssn);
+}
+
+/// <inheritdoc cref="ISsnValidator"/> 
+public class SsnValidator : ISsnValidator
+{
+    /// <summary>
+    /// Dictionary of valid checksum characters for Finnish social security number.
+    /// </summary>
+    private static readonly Dictionary<int, char> ChecksumDictionary = new()
+    {
+        { 0, '0' },
+        { 1, '1' },
+        { 2, '2' },
+        { 3, '3' },
+        { 4, '4' },
+        { 5, '5' },
+        { 6, '6' },
+        { 7, '7' },
+        { 8, '8' },
+        { 9, '9' },
+        { 10, 'A' },
+        { 11, 'B' },
+        { 12, 'C' },
+        { 13, 'D' },
+        { 14, 'E' },
+        { 15, 'F' },
+        { 16, 'H' },
+        { 17, 'J' },
+        { 18, 'K' },
+        { 19, 'L' },
+        { 20, 'M' },
+        { 21, 'N' },
+        { 22, 'P' },
+        { 23, 'R' },
+        { 24, 'S' },
+        { 25, 'T' },
+        { 26, 'U' },
+        { 27, 'V' },
+        { 28, 'W' },
+        { 29, 'X' },
+        { 30, 'Y' }
+    };
+    
+    public bool Validate(string? ssn)
+    {
+        /*
+         * Valid Finnish social security number is in the format:
+         * PPKKVVXNNNT
+         * Where:
+         * - PP = day of birth (01-31)
+         * - KK = month of birth (01-12)
+         * - VV = two last digits of year of birth (00-99)
+         * - X = century separator character (A-F, Y-U, +) indicating century of birth 1800, 1900 or 2000
+         * - NNN = serial number (002-899)
+         *   - Even for women
+         *   - Odd for men
+         *   - Values 900 - 999 are reserved for testing purposes and are not generally considered as valid values
+         * - T = checksum character (0-9, A-Y) which is calculated as PPKKVVNNN % 31 and then reminder dictates the checksum with fixed values seen in _checksumDictionary
+         */
+        
+        // Empty ssn is obviously invalid
+        if(string.IsNullOrEmpty(ssn))
+        {
+            return false;
+        }
+        
+        // Finnish ssn is 11 characters long
+        if(ssn.Length != 11)
+        {
+            return false;
+        }
+        
+        var pp = ssn[..2];
+        var kk = ssn[2..4];
+        var vv = ssn[4..6];
+
+        if (!int.TryParse(pp, out var day))
+        {
+            return false;
+        }
+        if (!int.TryParse(kk, out var month))
+        {
+            return false;
+        }
+        if (!int.TryParse(vv, out var year))
+        {
+            return false;
+        }
+
+        if (!TryParseCentury(ssn[6], out var century))
+        {
+            return false;
+        }
+        var yearOfBirth = century + year;
+        var nnnStr = ssn[7..10];
+        if (!int.TryParse(nnnStr, out var nnn) || nnn < 2 || nnn > 999)
+        {
+            return false;
+        }
+        var ppkkvvnnn = int.Parse(pp + kk + vv + nnnStr);
+        var checksumChar = ssn[10];
+        if(!TryCalculateChecksum(ppkkvvnnn, out var calculatedChecksumChar) || 
+           checksumChar != calculatedChecksumChar)
+        {
+            return false;
+        }
+
+        return true;
+    }
+    
+    
+        
+
+    /// <summary>
+    /// Finnish Social security number allows following century separator characters:
+    ///
+    /// <list type="bullet">
+    ///<item>A, B, C, D, E, F for 2000s</item>
+    ///<item>Y, X, W, V, U for 1900s</item>
+    ///<item>+ for 1800s</item>
+    /// </list>
+    /// </summary>
+    /// <param name="centuryChar">Century character</param>
+    /// <param name="century">Output variable for parsed century: 1800 or 1900 or 2000 for valid century, -1 otherwise</param>
+    /// <returns>True if valid century character, false otherwise</returns>
+    private static bool TryParseCentury(char centuryChar, out int century)
+    {
+        switch (centuryChar)
+        {
+            case 'A':
+            case 'B':
+            case 'C':
+            case 'D':
+            case 'E':
+            case 'F':
+                century = 2000;
+                return true;
+            case 'Y':
+            case 'X':
+            case 'W':
+            case 'V':
+            case 'U':
+                century = 1900;
+                return true;
+            case '+':
+                century = 1800;
+                return true;
+            default:
+                century = -1;
+                return false;
+        }
+    }
+    
+    /// <summary>
+    /// Try parse/calculate the checksum character for the given ppkkvvnnn part of Finnish social security number.
+    /// </summary>
+    /// <param name="ppkkvvnnn">ppkkvvnnn from ssn</param>
+    /// <param name="checksumChar">Output variable for parsed checksum char for valid ssns, otherwise default</param>
+    /// <returns>True if </returns>
+    private static bool TryCalculateChecksum(int ppkkvvnnn, out char checksumChar)
+    {
+        var checksum = ppkkvvnnn % 31;
+        return ChecksumDictionary.TryGetValue(checksum, out checksumChar);
+    }
+}
